bad3f24bada7bb7ea0ab09663a70871f404f462c
LaFlama777 <crosvetandjhack@gmail.com>
2025-10-06 11:56:05 +0700

    feat: Integrasi supabase postgreSQL

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import datetime
 
 
-DATABASE_URL = "sqlite:///./dlp_offenses.db" 
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
